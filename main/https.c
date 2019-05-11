cfd1dadd1ff989083d3128fc1f3eff59e19284bc
jitenshap <private.s.kobayashi@gmail.com>
2019-05-11 22:25:01 +0900

    add comment

--- main/https.c
+++ main/https.c
@@ -16,7 +16,7 @@
 #include "esp_event.h"
 #include "tcpip_adapter.h"
 
-#include "esp_http_client.h"
+#include "esp_http_client.h"	//かなり高機能なhttpクライアント
 
 #include "sdkconfig.h"
 #include "esp_event.h"
@@ -41,13 +41,16 @@
    To embed it in the app binary, the PEM file is named
    in the component.mk COMPONENT_EMBED_TXTFILES variable.
 */
+
+//テストのための公開キーを設定
 extern const char howsmyssl_com_root_cert_pem_start[] asm("_binary_howsmyssl_com_root_cert_pem_start");
 extern const char howsmyssl_com_root_cert_pem_end[]   asm("_binary_howsmyssl_com_root_cert_pem_end");
 
-static const char *TAG = "HTTPS";
+static const char *TAG = "HTTPS";	//ESP_LOGIとかやったときに表示させるヘッダ
 
-EventGroupHandle_t https_task_event_group;
+EventGroupHandle_t https_task_event_group;	//SetBitsのためのイベントグループ
 
+/*`	httpクライアントのイベントハンドラ	*/
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
     switch(evt->event_id) 
@@ -83,37 +86,38 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
     return ESP_OK;
 }
 
-
+/*	https get	*/
 void https_with_url()
 {
+    //httpsクライアントの設定変数
     esp_http_client_config_t config = 
 	{
         .url = "https://www.howsmyssl.com",
         .event_handler = _http_event_handler,
         .cert_pem = howsmyssl_com_root_cert_pem_start,
+	.port = 443,
     };
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_err_t err = esp_http_client_perform(client);
+    esp_http_client_handle_t client = esp_http_client_init(&config);	//httpsクライアントを作成、設定を適用
+    esp_err_t err = esp_http_client_perform(client);	//httpsクライアント実行
 
     if (err == ESP_OK) 
-	{
+    {
         ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %d",
                 esp_http_client_get_status_code(client),
                 esp_http_client_get_content_length(client));
-    } 
-	else 
-	{
+    }
+    else 
+    {
         ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
     }
-    esp_http_client_cleanup(client);
+    esp_http_client_cleanup(client);	//クライアントのバッファを開放
 }
 
 
 void https_test_task(void *pvParameters)
 {
-    https_with_url();
-
+    https_with_url();	
     ESP_LOGI(TAG, "Finish http example");
-	xEventGroupSetBits(https_task_event_group, GET_TASK_END_BIT);
-    vTaskDelete(NULL);
+    xEventGroupSetBits(https_task_event_group, GET_TASK_END_BIT);	//終了フラグを立てる
+    vTaskDelete(NULL);	//このタスクを終了
 }
